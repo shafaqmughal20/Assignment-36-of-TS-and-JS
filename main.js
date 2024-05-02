eae884d11ff10d1439f188f7c58747196242b84e
Shafaq Mughal <160257641+shafaqmughal20@users.noreply.github.com>
2024-05-02 08:30:27 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+//36.T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+function make_shirt(size, printMessage) {
+    console.log("you selecte ".concat(size, " size shirt with ").concat(printMessage, " print on shirt"));
+}
+make_shirt("small", "good human");
+make_shirt("small", "love palestine");
+make_shirt("small", "good heart muslim");
---
+++ main.ts
@@ -0,0 +1,9 @@
+//36.T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+
+function make_shirt(size :string,printMessage:string) {console.log(`you selecte ${size} size shirt with ${printMessage} print on shirt`)
+    
+}
+
+make_shirt("small" , "good human")
+
+
